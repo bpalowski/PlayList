517d09236be92dc1b7c64fc4d8eeac1530ccab79
b.palowski <brianpalowski00@gmail.com>
2019-02-18 01:38:02 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,56 @@
 import React, { Component } from 'react';
+import './App.css';
 
+class First extends Component {
+  render() {
+    return (
+      <div className="title" id="title">
+        <h2>Hello 2</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return (
+      <div>
+        <img />
+        <input type="text" />
+      </div>
+    );
+  }
+}
+class PlayList extends Component {
+  render() {
+    return (
+      <div id="playlist">
+        <img />
+        <h3>PlayList Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
 
+
+
   render() {
-    let pos = 'center';
     return (
-      <div >
-        <h1 style={
-          {
-            textAlign: pos
-          }
-        }>Hello</h1>
+      <div id="app1" >
+        <h1>Hello</h1>
+        <First />
+        <First />
+        <Filter />
+        <PlayList />
+        <PlayList />
+        <PlayList />
       </div >
     );
   }
